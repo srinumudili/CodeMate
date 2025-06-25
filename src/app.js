4a8796858c007666121bd84368b5be2912e0a256
srinumudili <sekharsrinu200@gmail.com>
2025-06-25 11:35:11 +0530

    Handling multiple route handles

--- src/app.js
+++ src/app.js
@@ -1,16 +1,33 @@
 const express = require("express");
 const app = express();
 
-// app.use("/test", (req, res) => {
-//   res.send("Hello World");
-// });
-
-app.get("/user", (req, res) => {
-  res.send({ firstname: "Srinu", lastname: "mudili" });
-});
-app.post("/user", (req, res) => {
-  res.send("Data successfully saved into db..");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Request handler!!");
+    // res.send("Response!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Request handler2!!");
+    // res.send("2nd Response!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Request handler3!!");
+    // res.send("3nd Response!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Request handler4!!");
+    // res.send("4th Response!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Request handler5!!");
+    res.send("5th Response!");
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000...");
