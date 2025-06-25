41d62081c43112c01ac2b955385546acecfaba6a
srinumudili <sekharsrinu200@gmail.com>
2025-06-25 11:04:23 +0530

    explore get,post methods

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("Hello World");
+// app.use("/test", (req, res) => {
+//   res.send("Hello World");
+// });
+
+app.get("/user", (req, res) => {
+  res.send({ firstname: "Srinu", lastname: "mudili" });
+});
+app.post("/user", (req, res) => {
+  res.send("Data successfully saved into db..");
 });
 
 app.listen(3000, () => {
