598b01d770ae3aca9c616d54df72cfc2bd3e51d2
srinumudili <sekharsrinu200@gmail.com>
2025-07-09 12:25:39 +0530

    user/feed api route

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 const router = express.Router();
 
 router.post("/request/send/:status/:toUserId", userAuth, async (req, res) => {
@@ -12,6 +13,11 @@ router.post("/request/send/:status/:toUserId", userAuth, async (req, res) => {
 
     if (!allowedStatus.includes(status)) {
       return res.status(400).json({ message: `Invalid Status Type ${status}` });
+    }
+
+    const toUser = await User.findById(toUserId);
+    if (!toUser) {
+      return res.status(404).json({ message: "User NOT Found." });
     }
 
     const existingConnectionRequest = await ConnectionRequest.findOne({
@@ -30,12 +36,52 @@ router.post("/request/send/:status/:toUserId", userAuth, async (req, res) => {
       toUserId,
       status,
     });
-    data = await connectionRequest.save();
+    const data = await connectionRequest.save();
 
-    res.json({ message: "Connection Request sent successfully!!" });
+    res.json({ message: "Connection Request sent successfully!!", data });
   } catch (error) {
     res.status(400).send(`ERROR : ${error.message}`);
   }
 });
+
+router.post(
+  "/request/review/:status/:requestId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const loggedInUser = req.user;
+      const { status, requestId } = req.params;
+
+      const allowedStatus = ["accepted", "rejected"];
+      if (!allowedStatus.includes(status)) {
+        return res
+          .status(400)
+          .json({ message: `Invalid status type: ${status}` });
+      }
+
+      const connectionRequest = await ConnectionRequest.findOne({
+        _id: requestId,
+        toUserId: loggedInUser._id,
+        status: "interested",
+      });
+
+      if (!connectionRequest) {
+        return res.status(404).json({
+          message: "Connection Request not found or already reviewed",
+        });
+      }
+
+      connectionRequest.status = status;
+      const data = await connectionRequest.save();
+
+      res.status(200).json({
+        message: `Connection Request has been ${status}`,
+        data,
+      });
+    } catch (error) {
+      res.status(500).send(`ERROR: ${error.message}`);
+    }
+  }
+);
 
 module.exports = router;
