9e7ff6372326d020f7ff13b52ef0e3204b5325a0
srinumudili <sekharsrinu200@gmail.com>
2025-07-28 12:13:39 +0530

    fixed database connection bug

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  console.log("Connecting to MongoDB:", process.env.DATABASE_URI);
   await mongoose.connect(process.env.DATABASE_URI);
 };
 
